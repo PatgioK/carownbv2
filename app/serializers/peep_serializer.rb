313ea8381391254f5a5326c5f75ad979a10747b8
Pat <Patgiok@gmail.com>
2022-03-20 16:39:02 -0700

    add relationship to serializer and set routes

--- app/serializers/peep_serializer.rb
+++ app/serializers/peep_serializer.rb
@@ -1,4 +1,6 @@
 class PeepSerializer
   include JSONAPI::Serializer
   attributes :firstname, :lastname, :email, :slug
+
+  has_many :cars
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  resources :cars
-  resources :peeps
+  # resources :cars
+  # resources :peeps
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  root 'pages#index'
+  namespace :api do
+    namespace :v1 do
+      resources :peeps, param: :slug
+      resources :cars
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
+
 end
